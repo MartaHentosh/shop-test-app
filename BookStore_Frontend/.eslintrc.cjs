72e4aeeaa9c31c01708e0e588ce2835c8af87567
iviul <iviul@outlook.com>
2025-12-29 14:07:09 +0200

    Action

--- BookStore_Frontend/.eslintrc.cjs
+++ BookStore_Frontend/.eslintrc.cjs
@@ -10,11 +10,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react-refresh/only-export-components': 'off',
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
+    'no-extra-semi': 'warn',
+    'prefer-const': 'warn',
   },
 }
